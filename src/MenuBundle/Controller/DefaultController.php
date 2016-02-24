59f82366ca97027c9ff51e8f567250373e6f5d04
laynos <laynosb@gmail.com>
2016-02-24 20:30:49 +0100

    working on Json post

--- src/MenuBundle/Controller/DefaultController.php
+++ src/MenuBundle/Controller/DefaultController.php
@@ -5,8 +5,7 @@ namespace MenuBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Facebook\FacebookRequest;
 use Facebook\Facebook;
-use Facebook\Authentication\AccessToken;
-
+use Facebook\FacebookApp;
 
 class DefaultController extends Controller
 {
@@ -17,15 +16,36 @@ class DefaultController extends Controller
     }
 	
 	public function menuAction(){
-	$request = new FacebookRequest(
-			$session,
-			'GET',
-			'/515562508539711/posts'
-		);
-
-		$response = $request->execute();
-		$graphObject = $response->getGraphObject();
-	/* handle the result */
-		return $this->render('MenuBundle:Site:menu.html.twig');
+		$data  = file_get_contents("https://graph.facebook.com/515562508539711/feed?access_token=CAACEdEose0cBAIwZCQ5zFGR8shEmZBEEPdxU6nIvoNNqrAzpqgACFJcEBZCzTUhnpmDkGtGnfQ7GsRy3qfglRj57yXHBW2m3ygZBKzoCPI7FuSAoRUERugmNOcxZB5M9AQViB6LSs2KrrzaduAZCnihL4m5ZCaHqmRyapHmYtZBKI2etRF9ksfKSXcKLhYRVyfWfbnBSGzTeixAjMVHl7Dze");
+		$data = json_decode($data);
+		var_dump($data);
+		$flag = 0;
+		$i = 0;
+		$array;
+		
+		/*while($flag < 3){
+			if(isset($data->{'data'}[$i]->{'message'})){
+				$message = $data->{'data'}[$i]->{'message'};
+				
+				//if(($msg = strstr($message, 'Menu', false)) !== null ){
+				if(strpos($message,'Menu')){
+					echo $flag;
+					$array[$flag] = $msg;
+					echo $array[$flag];
+					$flag++;
+					echo $flag;
+				}
+			}
+			$i++;
+		}*/
+	
+		/*for ($i = 0; $i <= 100; $i++) {
+			$message = $data->{'data'}[$i]->{'message'};
+			echo $message;
+		}*/
+		//return $this->render('MenuBundle:Site:menu.html.twig');
+		 return $this->render('MenuBundle:Site:menu.html.twig', array(
+      'data' => $data
+    ));
 	}
 }
