8f63859afc8db9e10da44aad305ae02a136d8b76
laynos <laynosb@gmail.com>
2016-02-29 11:17:43 +0100

    final commit

--- src/MenuBundle/Controller/DefaultController.php
+++ src/MenuBundle/Controller/DefaultController.php
@@ -3,17 +3,14 @@
 namespace MenuBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Facebook\Authentication\AccessToken;
+use Facebook\FacebookApp;
 
 class DefaultController extends Controller
 {
-   /* public function indexAction()
-    {
-       // return new Response("Hello world!");
-		return $this->render('MenuBundle:Default:index.html.twig');
-    }
-	*/
 	public function ilotAction(){
-		$accessToken = 'CAACEdEose0cBACa1XUBbImeZBl8LpHFOCzUwg0ZBx5ZAns7oT4urWIhyObHzJWJG1nMewPTu66cCkN4RRgB2FLYRNmkTKnGz3LPaMcZC76DDgUbXv6pmg0oHZA31ayFZAwEAJMP1MoLOaMeJm5hHZCIH2ixITwEgq1R8Wbd41ZB802B57K83qVXkaUiMwbO2gOneZBPx95PiM5wZAuyTg6JLyT';
+		$fbApp = new FacebookApp('1717060985178599', 'd352d24d855b9ad976621c0b7ed8f05e');
+		$accessToken = $fbApp->getAccessToken();
 		$data1  = file_get_contents("https://graph.facebook.com/515562508539711/posts?access_token=$accessToken");
 		$data1 = json_decode($data1);
 		$flag = 0;
@@ -42,8 +39,9 @@ class DefaultController extends Controller
 	}
 	
 		public function regalAction(){
-		$accessToken = 'CAACEdEose0cBACa1XUBbImeZBl8LpHFOCzUwg0ZBx5ZAns7oT4urWIhyObHzJWJG1nMewPTu66cCkN4RRgB2FLYRNmkTKnGz3LPaMcZC76DDgUbXv6pmg0oHZA31ayFZAwEAJMP1MoLOaMeJm5hHZCIH2ixITwEgq1R8Wbd41ZB802B57K83qVXkaUiMwbO2gOneZBPx95PiM5wZAuyTg6JLyT';
-		$data2 = file_get_contents("https://graph.facebook.com/649823778452363/posts?access_token=$accessToken");
+		$fbApp = new FacebookApp('1717060985178599', 'd352d24d855b9ad976621c0b7ed8f05e');
+		$accessToken = $fbApp->getAccessToken();
+		$data2  = file_get_contents("https://graph.facebook.com/649823778452363/posts?access_token=$accessToken");
 		$data2 = json_decode($data2);
 		$i = 0;
 		$flag = 0;
@@ -72,10 +70,11 @@ class DefaultController extends Controller
 	}
 	
 	public function menuAction(){
-		$accessToken = 'CAACEdEose0cBACa1XUBbImeZBl8LpHFOCzUwg0ZBx5ZAns7oT4urWIhyObHzJWJG1nMewPTu66cCkN4RRgB2FLYRNmkTKnGz3LPaMcZC76DDgUbXv6pmg0oHZA31ayFZAwEAJMP1MoLOaMeJm5hHZCIH2ixITwEgq1R8Wbd41ZB802B57K83qVXkaUiMwbO2gOneZBPx95PiM5wZAuyTg6JLyT';
+		$fbApp = new FacebookApp('1717060985178599', 'd352d24d855b9ad976621c0b7ed8f05e');
+		$accessToken = $fbApp->getAccessToken();
 		$data1  = file_get_contents("https://graph.facebook.com/515562508539711/posts?access_token=$accessToken");
 		$data1 = json_decode($data1);
-		//var_dump($data1);
+
 		$flag = 0;
 		$i = 0;
 		$ilot;
@@ -111,7 +110,7 @@ class DefaultController extends Controller
 					//echo $flag;
 					$regal[$flag] = $message;
 					//echo $regal[$flag];
-				//	echo "\n";
+					//echo "\n";
 					$flag++;
 					//echo $flag;
 				}
